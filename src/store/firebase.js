d6c025e9d07daf6238c72c7ad1f2a86a5c5d09c5
Jan Florian Dietrich <jfd@daenen4.de>
2016-10-16 11:14:28 +0200

    doc: Point out problem with code for future reference

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -2,6 +2,12 @@ import Firebase from 'firebase'
 
 import config from '../../config/'
 
+// TODO: find a better solution for this
+/* Maybe this can help
+ firebase.app().delete().then(function() {
+                console.log("[DEFAULT] App is Gone Now");
+            });
+ */
 const firebaseApp = Firebase.initializeApp(config.dev.firebase, `app_${Date.now()}`)
 const db = firebaseApp.database()
 
